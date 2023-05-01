0594708539b72c8b49df5483a7592c0263bf22c2
metinars <arsmetin90@gmail.com>
2023-05-01 21:11:02 +0300

    bug fix

--- assets/scripts/blogAdd.js
+++ assets/scripts/blogAdd.js
@@ -1,46 +1,51 @@
 const userSignForm = document.querySelector('#form-signup');
 const blogAddForm = document.querySelector('#new-post')
 
+if (blogAddForm) {
+    blogAddForm.addEventListener('submit', (event) => {
+        event.preventDefault();
 
-blogAddForm.addEventListener('submit', (event) => {
-    event.preventDefault();
+        const enteredTitle = event.currentTarget.querySelector('#title').value;
+        const enteredShortContent = event.currentTarget.querySelector('#short').value;
+        const enteredLongContent = event.currentTarget.querySelector('#long').value;
 
-    const enteredTitle = event.currentTarget.querySelector('#title').value;
-    const enteredShortContent = event.currentTarget.querySelector('#short').value;
-    const enteredLongContent = event.currentTarget.querySelector('#long').value;
+        fetch('http://localhost:3000/admin/add', {
+            method: 'POST',
+            body: JSON.stringify({
+                title: enteredTitle,
+                short: enteredShortContent,
+                long: enteredLongContent,
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then((response) => {
+            return response.json();
+        })
 
-    fetch('http://localhost:3000/admin/add', {
-        method: 'POST',
-        body: JSON.stringify({
-            title: enteredTitle,
-            short: enteredShortContent,
-            long: enteredLongContent,
-        }),
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    }).then((response) => {
-        return response.json();
     })
+}
 
-})
-userSignForm.addEventListener('submit', (event) => {
-    event.preventDefault();
+if (userSignForm) {
+    userSignForm.addEventListener('submit', (event) => {
+        event.preventDefault();
 
-    const enteredUserName = event.currentTarget.querySelector('#username').value;
-    const enteredUserPassword = event.currentTarget.querySelector('#password').value;
+        const enteredUserName = event.currentTarget.querySelector('#username').value;
+        const enteredUserPassword = event.currentTarget.querySelector('#password').value;
 
-    fetch('http://localhost:3000/signup', {
-        method: 'POST',
-        body: JSON.stringify({
-            userName: enteredUserName,
-            password: enteredUserPassword,
-        }),
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    }).then((response) => {
-        return response.json();
+        fetch('http://localhost:3000/signup', {
+            method: 'POST',
+            body: JSON.stringify({
+                userName: enteredUserName,
+                password: enteredUserPassword,
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then((response) => {
+            return response.json();
+        })
+
     })
+}
 
-})
